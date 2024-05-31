02ff41a673265086a90e022f1e5dc19091f4aa52
namnguy1 <namepchai1221@gmail.com>
2024-05-31 09:39:30 +0700

    register has not completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,11 @@
-const { User } = require('../models/User');
+const { User } = require('../models');
 
 class UserController {
   register = async (req, res, next) => {
     try {
+      const alreadyExistUser = await User.findOne({ where: { email: req.body.email }})
+      if (alreadyExistUser) 
+          return res.status(400).json({ Message: "Email is already existed!" })
       const user = await User.create(req.body);
       res.status(201).json(user);
     } catch (error) {
